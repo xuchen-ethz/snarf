1bb4895a3f5d2483fb3d5ae63453e5d0e3ac5ecf
Xu Chen <xuchen@inf.ethz.ch>
2021-09-27 21:21:34 +0200

    init commit for project page

--- lib/model/metrics.py
+++
@@ -1,6 +0,0 @@
-import torch
-
-def calculate_iou(gt, prediction):
-    intersection = torch.logical_and(gt, prediction)
-    union = torch.logical_or(gt, prediction)
-    return torch.sum(intersection) / torch.sum(union)
